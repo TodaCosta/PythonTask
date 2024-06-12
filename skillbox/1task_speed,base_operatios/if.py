9c57fbebc26fae542662491b4f880bea78374c19
Irina Brezgunova <irina91git@mail.ru>
2024-06-12 23:39:19 +0300

    while_1

--- skillbox/1task_speed,base_operatios/if.py
+++ skillbox/1task_speed,base_operatios/if.py
@@ -1,3 +1,10 @@
 # if - условный оператор - ветвление
 a = 6
 print(8 if a + 2 == 8 else 0)
+
+
+x = int(input('Введите число: '))
+modul_x = x if x > 0 else -x
+print('ввели ' +str(x), ', ответ ' + str(modul_x))
+
+#https://habr.com/ru/articles/718980/
